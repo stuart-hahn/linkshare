3fa53c2822b0a8d9b203ee8e16f4ac2330212d70
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-12 12:06:45 -0500

    add ability to sign in through github using devise and omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, '4b0d3441401be6ac4245', 'c2e8910e4a40e5df4d2e8e633491385428711cf3', scope: 'user:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root "links#index"
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   
-  devise_for :users
   resources :categories
   resources :links
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
