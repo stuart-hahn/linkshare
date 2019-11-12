89f5ef6223c1193caed5bdc29244ad825b747072
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-12 11:10:37 -0500

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+test = Category.create(title: "test")
+youtube = Link.create(title: "YouTube", url: "https://www.youtube.com/", category: test, user_id: 1)
