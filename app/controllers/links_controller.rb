b34532869fa9bac3f5bf9f2ac2cc41043eb4e281
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-12 18:53:30 -0500

    temporarily unnest routes to test edit functionality

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,7 +3,7 @@ class LinksController < ApplicationController
     skip_before_action :authenticate_user!, only: [:index]
 
     def index
-        if there_is_category?
+        if params[:category_id] && @category = Category.find_by(id: params[:category_id])
             @links = @category.links
         else
             flash.now[:alert] = "That category doesn't exist" if params[:category_id]
@@ -12,7 +12,7 @@ class LinksController < ApplicationController
     end
 
     def new
-        if there_is_category?
+        if params[:category_id] && @category = Category.find_by(id: params[:category_id])
             @link = @category.links.build
         else
             @link = Link.new
@@ -29,10 +29,23 @@ class LinksController < ApplicationController
         end
     end
 
-    def destroy
+    def edit
         @link = Link.find_by(id: params[:id])
-        if current_user.owns_link?(@link)
-            @link.destroy
+    end
+
+    def update
+        @link = Link.find_by(id: params[:id])
+        if @link.update(link_params)
+            redirect_to link_path(@link)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        link = Link.find_by(id: params[:id])
+        if current_user.owns_link?(link)
+            link.destroy
             redirect_to root_path, notice: "Link successfully deleted"
         else
             flash[:error] = "You cannot delete that link"
@@ -48,9 +61,5 @@ class LinksController < ApplicationController
 
     def link_params
         params.require(:link).permit(:title, :url, :category_id)
-    end
-
-    def there_is_category?
-        params[:category_id] && @category = Category.find_by(id: params[:category_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   
-  resources :links, only: [:new, :create, :index]
+  resources :links
 
   resources :categories do
-    resources :links, shallow: true
+    resources :links
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
