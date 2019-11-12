e71de32adf6289dfcdcc690d1a394376c1735403
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-12 12:55:49 -0500

    set authenticate_user! on every page except for the links index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    # Provided by Devise - authenticates a user or redirects them to login page
+    before_action :authenticate_user!
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,5 @@
 class LinksController < ApplicationController
+    skip_before_action :authenticate_user!, only: :index
     def index
         @links = Link.all
     end
