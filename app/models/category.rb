526a884ca59e6563b8ce2dbdcffcf274f2793487
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-12 12:22:36 -0500

    add validations to category and link, update spec.md

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
     has_many :links
     has_many :users, through: :links
+
+    validates :title, presence: true, uniqueness: { case_sensitive: false }
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :category
   belongs_to :user
+
+  validates :title, presence: true
+  validates :url, presence: true
 end
