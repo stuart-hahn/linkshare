ba3d12369badcffcd1126938455a1f5868834f6f
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-12 13:33:23 -0500

    get authenticate_user! properly set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-    # Provided by Devise - authenticates a user or redirects them to login page
     before_action :authenticate_user!
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,26 @@
 class LinksController < ApplicationController
-    skip_before_action :authenticate_user!, only: :index
+
+    skip_before_action :authenticate_user!, only: [:index]
     def index
         @links = Link.all
+    end
+
+    def new
+        @link = Link.new
+    end
+
+    def create
+        @link = Link.new(link_params)
+        if @link.save
+            redirect_to link_path(@link), notice: "Link successfully created"
+        else
+            render :new, alert: "Failed to created link"
+        end
+    end
+
+    private
+
+    def link_params
+        params.require(:link).permit(:title, :url, :category_id)
     end
 end
