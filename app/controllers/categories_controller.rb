c97ee5eac707ba4166ed5c5c9ff3925564ee0396
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-13 13:09:22 -0500

    refactor category controller and remove UD from CRUD

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,24 +20,27 @@ class CategoriesController < ApplicationController
         end
     end
 
-    def edit
-        @category = Category.find_by(id: params[:id])
-    end
+    ### The UD part of CRUD is unnecessary here, I don't want
+    ### people to be able to edit or delete entire categories
 
-    def update
-        @category = Category.find_by(id: params[:id])
-        if @category.update(category_params)
-            redirect_to category_path(@category), notice: "Category succesfully updated"
-        else
-            render :edit, alert: "Failed to update category"
-        end
-    end
+    # def edit
+    #     @category = Category.find_by(id: params[:id])
+    # end
 
-    def destroy
-        @category = Category.find_by(id: params[:id])
-        @category.delete
-        redirect_to categories_path, notice: "Category successfully deleted"
-    end
+    # def update
+    #     @category = Category.find_by(id: params[:id])
+    #     if @category.update(category_params)
+    #         redirect_to category_path(@category), notice: "Category succesfully updated"
+    #     else
+    #         render :edit, alert: "Failed to update category"
+    #     end
+    # end
+
+    # def destroy
+    #     @category = Category.find_by(id: params[:id])
+    #     @category.delete
+    #     redirect_to categories_path, notice: "Category successfully deleted"
+    # end
 
     private
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
-  resources :categories
+  resources :categories, only: [:new, :show, :index] do
+    resources :links
+  end
   resources :links
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
