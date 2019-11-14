61d016f6612c0dd9240d2a1deb696cf26854f94d
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-14 10:14:37 -0500

    add vote associations and display points on link index

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  has_many :votes
 
   validates :title, presence: true
   validates :url, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :links
   has_many :categories
   has_many :used_categories, through: :links, source: :category
+  has_many :votes
 
   def self.from_omniauth(auth)  
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
