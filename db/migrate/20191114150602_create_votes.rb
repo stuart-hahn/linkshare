37ca1092fd9c55e8316b69d420a95627662d2091
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-14 10:10:13 -0500

    add default values to votes, points, and trending

--- db/migrate/20191114150602_create_votes.rb
+++ db/migrate/20191114150602_create_votes.rb
@@ -3,8 +3,8 @@ class CreateVotes < ActiveRecord::Migration[6.0]
     create_table :votes do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :link, null: false, foreign_key: true
-      t.integer :upvote
-      t.integer :downvote
+      t.integer :upvote, default: 0
+      t.integer :downvote, default: 0
 
       t.timestamps
     end
--- db/migrate/20191114150815_add_points_and_trending_to_link.rb
+++ db/migrate/20191114150815_add_points_and_trending_to_link.rb
@@ -1,6 +1,6 @@
 class AddPointsAndTrendingToLink < ActiveRecord::Migration[6.0]
   def change
-    add_column :links, :points, :integer
-    add_column :links, :trending, :float
+    add_column :links, :points, :integer, default: 1
+    add_column :links, :trending, :float, default: 0
   end
 end
