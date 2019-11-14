9aa9bc7bff24d9e87700235629a0334175a934eb
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2019-11-14 10:16:39 -0500

    add links/:id/upvote route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   resources :categories do
     resources :links
   end
-  resources :links
+  
+  resources :links do
+    post :upvote, on: :member
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
